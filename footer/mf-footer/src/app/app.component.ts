3035f31a0ecaac553d7cf92bdf3232c785e8d414
daniherr <daniherr@bancolombia.com.co>
2021-06-21 22:05:25 -0500

    subiendo todo lo ultimo

---
+++ footer/mf-footer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-footer',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mf-footer';
+}
---
+++ footer/mf-footer/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    EmptyRouteComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
